8157d16640b1be95cbe1d20f8a2363f7bfefd103
joao-vls <joa.v55j@gmail.com>
2023-08-15 10:19:08 -0300

    imagem

--- js/fcriar.js
+++ js/fcriar.js
@@ -1,47 +1,63 @@
-function dialogo(frases){
+function dialogo(frases) {
 
   for (var i = 0; i < frases.length; i++) {
-    var des=$(".descr:eq("+i+")");
+    var des = $(".descr:eq(" + i + ")");
     des.data("frase", frases[i]);
-    des.hover(function(e){
+    des.hover(function (e) {
       $("body").append(`<div class="caixa-di">
-      `+$(this).data("frase")+`
+      `+ $(this).data("frase") + `
       </div>`);
-      $(".caixa-di").css({left:(e.clientX-30)+"px",top:(e.clientY+20)+"px"})
+      $(".caixa-di").css({ left: (e.clientX - 30) + "px", top: (e.clientY + 20) + "px" })
     },
-    ()=>{
-      $(".caixa-di").remove();
-    });
+      () => {
+        $(".caixa-di").remove();
+      });
 
   }
 }
-function info_pessoa(){
+function multitexto(textos, ref) {
+  let tx = [''];
+  for (let i = 0; i < textos.length; i++) {
+    tx[0] += textos[i][ref[0]] + '<span class="descr" >?</span></br>';
+    if (ref[1]) {
+      tx[1 + i] = textos[i][ref[1]] + '</br>' + textos[i][ref[2]];
+    }
+  }
+  return tx;
+}
+function info_pessoa() {
+  let spcurso = multitexto(pessoa.cursos, ref = ['nome', 'local', 'ini_fim']);
   $(".content")?.remove();
   $(".projeto-carrosel")?.remove();
   $(".visor").append(`<div class="content">
   <div class="nome">
   <h2>Nome:</h2>
-  <p>`+pessoa.nome+`</p>
+  <p>`+ pessoa.nome + `</p>
   </div>
   <div class="ida">
   <h2>Idade:</h2>
-  <p>`+pessoa.idade+`<span class="descr" > ?</span> </p>
+  <p>`+ pessoa.idade() + `<span class="descr" > ?</span> </p>
   </div>
   <div class="cidades" >
   <h2>Cidades:</h2>
-  <p>`+pessoa.cidades[1].nome+` <span  class="descr" >?</span> || `+pessoa.cidades[0].nome+` <span class="descr" style="color: yellow;">?</span></p>
+  <p>`+ pessoa.cidades[1].nome + ` <span  class="descr" >?</span> || ` + pessoa.cidades[0].nome + ` <span class="descr" style="color: yellow;">?</span></p>
   </div>
   <div class="curs">
   <h2>Cursos:</h2>
-  <p>`+pessoa.cursos[0].nome+` <span class="descr" >?</span></p>
+  <p>`+ spcurso[0] + `</p>
   </div>
   </div>`);
-
-  dialogo(frases=[pessoa.nascimento,pessoa.cidades[1].descricao,pessoa.cidades[0].descricao,pessoa.cursos[0].local+"<br>"+pessoa.cursos[0].ini_fim]);
+  spcurso.shift();
+  console.log(spcurso);
+  let frases = [pessoa.nascimento, pessoa.cidades[1].descricao, pessoa.cidades[0].descricao];
+  spcurso.forEach(element => {
+    frases.push(element);
+  });
+  dialogo(frases);
 }
-function redes(){
+function redes() {
   $(".content")?.remove();
-   $(".projeto-carrosel")?.remove();
+  $(".projeto-carrosel")?.remove();
   $(".visor").append(`    <div class="content">
   <div class="grid">
   </div>
@@ -49,17 +65,17 @@ function redes(){
   </div>`)
   for (const chave in contatos) {
     $(".grid").append(`
-      <div class="rede">
-      <p>`+chave+`</p>
-      <a target="_blank" href="`+contatos[chave].link+`">`+contatos[chave].logo+`</a>
-      </div>
+      <a target="_blank" href="`+ contatos[chave].link + `" class="rede">
+      <p style="font-size:20px">`+ chave + `</p>
+      <p>` + contatos[chave].logo + `</p>
+      </a>
       `);
-    }
   }
-  function conheci(){
-    $(".content")?.remove();
-    $(".projeto-carrosel")?.remove();
-    $(".visor").append(`
+}
+function conheci() {
+  $(".content")?.remove();
+  $(".projeto-carrosel")?.remove();
+  $(".visor").append(`
       <div class="content">
       <table>
       <thead>
@@ -75,101 +91,122 @@ function redes(){
       </table>
       </div>
       `);
-      var nv10=`<div class="nivel">
+  var nv10 = `<div class="nivel">
       <i class="fa-regular fa-thumbs-down"></i>
       <div class="nv1a10 nv1"></div>
       `;
-      for (var i = 0; i < 8; i++) {
-        nv10+=`<div class="nv1a10"></div>
+  for (var i = 0; i < 8; i++) {
+    nv10 += `<div class="nv1a10"></div>
         `;
-      }
-      nv10+=`<div class="nv1a10 nv10"></div>
+  }
+  nv10 += `<div class="nv1a10 nv10"></div>
       <i class="fa-regular fa-thumbs-up"></i></div>`;
-      var cl=[],frases=[],nv=[],c=0,li=0;
-      for (const chave in conhecimentos) {
-        cl.push([]);
-        nv.push([]);
-        li=(li<conhecimentos[chave].length) ? conhecimentos[chave].length : li;
-        for (var i = 0; i < conhecimentos[chave].length; i++) {
+  var cl = [], frases = [], nv = [], c = 0, li = 0;
+  for (const chave in conhecimentos) {
+    cl.push([]);
+    nv.push([]);
+    li = (li < conhecimentos[chave].length) ? conhecimentos[chave].length : li;
+    for (var i = 0; i < conhecimentos[chave].length; i++) {
 
-          var conte=conhecimentos[chave][i].nome;
-          if (conhecimentos[chave][i]?.descricao) {
-            frases.push(conhecimentos[chave][i].descricao);
-            conte+=` <span class="descr" >?</span>`;
-          }
-          cl[c][i]=conte;
-          nv[c][i]=conhecimentos[chave][i].nivel
-          $("tbody").append('<tr></tr>');
-        }
-        c++;
+      var conte = conhecimentos[chave][i].nome;
+      if (conhecimentos[chave][i]?.descricao) {
+        frases.push(conhecimentos[chave][i].descricao);
+        conte += ` <span class="descr" >?</span>`;
       }
-      for (var i = 0; i < li; i++) {
-        for (var o = 0; o < c; o++) {
-          if (cl[o][i]) {
+      cl[c][i] = conte;
+      nv[c][i] = conhecimentos[chave][i].nivel
+      $("tbody").append('<tr></tr>');
+    }
+    c++;
+  }
+  for (var i = 0; i < li; i++) {
+    for (var o = 0; o < c; o++) {
+      if (cl[o][i]) {
 
-            $("tbody  tr:eq("+i+")").append(`
-              <td>`+(cl[o][i]+nv10)+`</td>
+        $("tbody  tr:eq(" + i + ")").append(`
+              <td>`+ (cl[o][i] + nv10) + `</td>
               `);
 
-              for (var n = 0; n < nv[o][i]; n++) {
-                $("tbody  tr:eq("+i+") td:eq("+o+") .nv1a10:eq("+n+")").css('background-color',"var(--cor-"+(n+1)+")");
-              }
-            }else {
-              $("tbody  tr:eq("+i+")").append(`
+        for (var n = 0; n < nv[o][i]; n++) {
+          $("tbody  tr:eq(" + i + ") td:eq(" + o + ") .nv1a10:eq(" + n + ")").css('background-color', "var(--cor-" + (n + 1) + ")");
+        }
+      } else {
+        $("tbody  tr:eq(" + i + ")").append(`
                 <td></td>
                 `);
-              }
+      }
 
-            }
-          }
-          dialogo(frases)
-        }
-        let carroselatual=0;
-        function projetcriar(){
-          var frases=[];
-          $(".projeto-carrosel p").text(Projetos[carroselatual].nome)
-          if(Projetos[carroselatual]?.defaultp){Projetos[0].defaultp();}
-          if (Projetos[carroselatual]?.link) {
-            $(".visor").append(`
+    }
+  }
+  dialogo(frases)
+}
+let carroselatual = 0;
+function projetcriar() {
+  var frases = [];
+  $(".projeto-carrosel p").text(Projetos[carroselatual].nome)
+  if (Projetos[carroselatual]?.defaultp) { Projetos[0].defaultp(); }
+  if (Projetos[carroselatual]?.link) {
+    $(".visor").append(`
               <div class="content">
-              <iframe src="`+Projetos[carroselatual].link+`" style="position:absolute;left:0;" width="99.5%" height="100%"></iframe>
+              <iframe src="`+ Projetos[carroselatual].link + `" style="position:absolute;left:0;" width="99.5%" height="100%"></iframe>
               </div>
               `)
-              $(".projeto-carrosel p").append(`<span class="descr" >?</span>`)
-              frases.push(Projetos[carroselatual].descricao+"<br>"+"Status: "+Projetos[carroselatual].status);
-            }
-            if (Projetos[carroselatual]?.disponivel) {
-              $(".visor").append(`    <div class="content">
+    $(".projeto-carrosel p").append(`<span class="descr" >?</span>`)
+    frases.push(Projetos[carroselatual].descricao + "<br>" + "Status: " + Projetos[carroselatual].status);
+  }
+  if (Projetos[carroselatual]?.disponivel) {
+    $(".visor").append(`    <div class="content">
               <div class="info" style="font-size:30px;">
-              <h2>`+Projetos[carroselatual].nome+`</h2>
-              <p>`+Projetos[carroselatual].descricao+`</p>
+              <h2>`+ Projetos[carroselatual].nome + `</h2>
+              <p>`+ Projetos[carroselatual].descricao + `</p>
               <p>Visualização indisponivel link abaixo para o repositorio</p>
               <p><i class="fa-solid fa-angle-down"></i></p>
-              <a href="`+Projetos[carroselatual].disponivel+`" style="font-size:100px;">
+              <a href="`+ Projetos[carroselatual].disponivel + `" style="font-size:100px;">
               <i class="fa-brands fa-github"></i>
               </a>
               </div>
               </div>`)
-            }
-            if (Projetos[carroselatual]?.pdfs) {
-              $(".visor").append(`    <div class="content">
+  }
+  if (Projetos[carroselatual]?.pdfs) {
+    $(".visor").append(`    <div class="content">
               <div class="grid">
               </div>
 
               </div>`)
-              for (var i = 0; i < Projetos[carroselatual].pdfs.length; i++) {
-                $(".grid").append(`<div class="pdf"> <p>`+ Projetos[carroselatual].pdfs[i].nome+`<span class="descr" > ?</span></p> <a target="_blank" href="`+Projetos[carroselatual].pdfs[i].link+`"><i class="fa-solid fa-book"></i></a></div>`)
-                frases.push(Projetos[carroselatual].pdfs[i].descricao);
+    for (var i = 0; i < Projetos[carroselatual].pdfs.length; i++) {
+      $(".grid").append(`<div class="pdim"> <p>` + Projetos[carroselatual].pdfs[i].nome + `<span class="descr" > ?</span></p> <a target="_blank" href="` + Projetos[carroselatual].pdfs[i].link + `"><i class="fa-solid fa-book"></i></a></div>`)
+      frases.push(Projetos[carroselatual].pdfs[i].descricao);
 
-              }
-            }
-            return frases;
-          }
-          function cproject(){
-            $(".content")?.remove();
-            $(".projeto-carrosel")?.remove();
-            Projetos[0].defaultp();
-            $(".opcoes").append(`
+    }
+  }
+  if (Projetos[carroselatual]?.imagens) {
+    $(".visor").append(`    <div class="content">
+    <div class="grid">
+    </div>
+    
+    </div>`)
+    for (var i = 0; i < Projetos[carroselatual].imagens.length; i++) {
+      $(".grid").append(`<div class="pdim" ><img style="float:left; width:100px; height:100px" src="` + Projetos[carroselatual].imagens[i].link + `"> <button  type="button" style="width:100px; height:100px; float: right;"  ><i class="fa-solid fa-maximize"></i></button> </div>`);
+      $(".pdim button:eq(" + i + ")").click(function () {
+        $("body").append(`  <div class="imagem-grande">
+        <button class="mop" style="text-align: center; background: none; border: none; cursor: pointer;"><i class="fa-solid fa-rectangle-xmark"></i></button>
+        <img class="imagem" src=""></div>
+      </div>`);
+        $(".imagem").attr("src",$(this).siblings("img").attr("src"));
+        $(".mop").click(() => {
+          $(".imagem-grande").remove();
+        });
+        
+      });
+    }
+  }
+  return frases;
+}
+function cproject() {
+  $(".content")?.remove();
+  $(".projeto-carrosel")?.remove();
+  Projetos[0].defaultp();
+  $(".opcoes").append(`
               <div class="projeto-carrosel">
               <button type="button" name="p-up"><i class="fa-solid fa-angle-up"></i></button>
               <p>Default</p>
@@ -177,14 +214,14 @@ function redes(){
 
               </div>
               `);
-              $('button[name="p-up"]').click(()=>{
-                $(".content")?.remove();
-                carroselatual=(carroselatual==Projetos.length-1) ? 0 :carroselatual+1;
-                dialogo(projetcriar());
-              });
-              $('button[name="p-down"]').click(()=>{
-                $(".content")?.remove();
-                carroselatual=(carroselatual==0) ? Projetos.length-1 :carroselatual-1;
-                dialogo(projetcriar());
-              });
-            }
+  $('button[name="p-up"]').click(() => {
+    $(".content")?.remove();
+    carroselatual = (carroselatual == Projetos.length - 1) ? 0 : carroselatual + 1;
+    dialogo(projetcriar());
+  });
+  $('button[name="p-down"]').click(() => {
+    $(".content")?.remove();
+    carroselatual = (carroselatual == 0) ? Projetos.length - 1 : carroselatual - 1;
+    dialogo(projetcriar());
+  });
+}
--- js/objetos.js
+++ js/objetos.js
@@ -1,6 +1,7 @@
-let Projetos=[{
-  'nome':"default",
-  'defaultp': function(){ $(".visor").append(`      <div class="content">
+let Projetos = [{
+  'nome': "default",
+  'defaultp': function () {
+    $(".visor").append(`      <div class="content">
           <div class="info" style="font-size:30px;">
             <p><i class="fa-solid fa-angle-up"></i></p>
             <p>Use os direcionais ao lado para mudar de projeto</p>
@@ -12,90 +13,105 @@ let Projetos=[{
           </div>
         </div>
       `);
-    }
-},{
-  'nome':"Domino",
-  'descricao':"Jogo de Domino feito com javascript",
+  }
+}, {
+  'nome': "Domino",
+  'descricao': "Jogo de Domino feito com javascript",
   'link': "https://joao-vls.github.io/jogo-domino-js-html/domino.html",
-  'status':"Completo"
+  'status': "Completo"
 },
 {
-  'nome':"One",
-  'descricao':"Layout semelhante ao spotify",
+  'nome': "One",
+  'descricao': "Layout semelhante ao spotify",
   'link': "https://joao-vls.github.io/one_layout_spotify/",
-  'status':"Completo"
+  'status': "Completo"
 },
 {
-  'nome':"Storm",
-  'descricao':"Projeto de rede social semelhante ao twitter com PHP",
-  'link': "https://joao-vls.github.io/storm-2023/versao_sem_BD/",
-  'status':"Incompleto"
+  'nome': "Storm",
+  'descricao': "Projeto de rede social semelhante ao twitter com PHP",
+  'link': "https://joao-vls.github.io/storm/versao_sem_BD/",
+  'status': "Incompleto"
 },
 {
-  'nome':"Jquery quizz",
-  'descricao':"Jogo de quizz sobre o Jquery feito em grupo",
+  'nome': "Jquery quizz",
+  'descricao': "Jogo de quizz sobre o Jquery feito em grupo",
   'link': "https://joao-vls.github.io/jquery_quizz/",
-  'status':"Completo"
+  'status': "Completo"
 },
 {
-  'nome':"Consultorio",
-  'descricao':"Projeto de consultorio medico que gerencia consultas e entrega resultados de consultas",
+  'nome': "Consultorio",
+  'descricao': "Projeto de consultorio medico que gerencia consultas e entrega resultados de consultas",
   'disponivel': `https://github.com/Joao-vls/consultorio-Php--incompleto--`,
-  'status':"Incompleto"
+  'status': "Incompleto"
 },
 {
-  'nome':"Exercicio 1",
-  'descricao':"Exercicio de carroseis",
+  'nome': "Exercicio 1",
+  'descricao': "Exercicio de carroseis",
   'link': "https://joao-vls.github.io/testes/arrayobjetos/arob.html",
-  'status':"Completo"
+  'status': "Completo"
 },
 {
-  'nome':"Exercicio 2",
-  'descricao':"Exercicio sobre imagens,iframes,e tags",
+  'nome': "Exercicio 2",
+  'descricao': "Exercicio sobre imagens,iframes,e tags",
   'link': "https://joao-vls.github.io/testes/continente/index.html",
-  'status':"Completo"
+  'status': "Completo"
 },
 {
-  'nome':"Exercicio 3",
-  'descricao':"Exercicio sobre trocar de modo claro para escuro,calculadora e busca interna",
+  'nome': "Exercicio 3",
+  'descricao': "Exercicio sobre trocar de modo claro para escuro,calculadora e busca interna",
   'link': "https://joao-vls.github.io/testes/mudar-modo-calculo-busca-interna/1pg.html",
-  'status':"Completo"
+  'status': "Completo"
 },
 {
-  'nome':"Textos(artigos)",
-  'descricao':"textos,artigos e projetos, produzidos",
-  'pdfs':[{'nome':"BOAS PRATICAS PARA GARANTIR A SEGURANÇA DAS INFORMAÇÕES.",'link':"./pdfs/sergurancainfo.pdf",'descricao':"Artigo desenvolvido para a disciplina de metodologia de pesquisa"}]
+  'nome': "Textos(artigos)",
+  'descricao': "textos,artigos e projetos, produzidos",
+  'pdfs': [{ 'nome': "BOAS PRATICAS PARA GARANTIR A SEGURANÇA DAS INFORMAÇÕES.", 'link': "./pdfs/sergurancainfo.pdf", 'descricao': "Artigo desenvolvido para a disciplina de metodologia de pesquisa" }]
+},
+{
+  'nome': "imagens(design)",
+  'descricao': "imagens e design produzidos",
+  'imagens': [{'link': "./img/imagens/_Con.png"},{'link': "./img/imagens/2.png"}
+  ,{'link': "./img/imagens/6.png"},{'link': "./img/imagens/10.png"},{'link': "./img/imagens/acolhe.png"},
+  {'link': "./img/imagens/fazen.png"},{'link': "./img/imagens/MINICURSO.png"},{'link': "./img/imagens/yot.png"},]
 }
 ];
-let pessoa={
-  'nome':"João vitor leite de silva",
-  'nascimento':"04/04/2003",
-  'idade':20,
-  'cidades':[{'nome':"São Paulo-SP",'descricao':"Local Nascimento"},{'nome':"Januaria-MG",'descricao':"Local atual"}],
-  'cursos':[{'nome':"Bacharelado em sistema da Informação",'local':"IFNMG-campus Januaria",'ini_fim':"2021,2025"}
-]
-};
-let conhecimentos={
-  'linguagenp':[{'nome':"Javascript",'nivel':7},
-  {'nome':"C/C++",'nivel':4},
-  {'nome':"PHP",'nivel':6},
-  {'nome':"R",'nivel':2},
-  {'nome':"Python",'nivel':2},
-  {'nome':"Java",'nivel':1}
-],
-  'bancodedados':[{'nome':"Mysql",'nivel':6},{'nome':"orientdb",'nivel':4},{'nome':"counchdb",'nivel':4}],
-  'linguas':[{'nome':"Inglês",'nivel':2},{'nome':"Espanhol",'nivel':5}],
-  'tecnicos':[
-  {'nome':"Manutenção e montagem de eletronicos",'nivel':7},
-  {'nome':"Engenharia de software",'nivel':9,'descricao':"metodologias ageis como kamban e scrum e outras tecnicas de desenvolvimento"},
-  {'nome':"Algoritmos",'nivel':10},
-  {'nome':"Estrutura de dados",'nivel':8},
-  {'nome':"Redes neurais",'nivel':2}
+let pessoa = {
+  'nome': "João vitor leite de silva",
+  'nascimento': "04/04/2003",
+  'idade': function () {
+    var ano = 2003, dia = 4, mes = 4;
+    var atual = new Date();
+    var idade = new Date(ano, mes - 1, dia);
+    var aid = atual - idade;
+
+    return Math.floor(aid / (1000 * 60 * 60 * 24 * 365.25));
+  },
+  'cidades': [{ 'nome': "São Paulo-SP", 'descricao': "Local Nascimento" }, { 'nome': "Januaria-MG", 'descricao': "Local atual" }],
+  'cursos': [{ 'nome': "Bacharelado em sistema da Informação", 'local': "IFNMG-campus Januaria", 'ini_fim': "2021,2025" },
+  { 'nome': "Machine Learning e Data Science com Python de A a Z", 'local': "Udemy", 'ini_fim': "2023,2023" }
   ]
 };
-let contatos={
-  'Instagram':{'link':"https://www.instagram.com/joa.vls/",'logo':`<i class="fa-brands fa-square-instagram"></i>`},
-  'Twitter':{'link':"https://twitter.com/joa_vvv",'logo':`<i class="fa-brands fa-square-twitter"></i>`},
-  'Github':{'link':"https://github.com/Joao-vls",'logo':`<i class="fa-brands fa-github"></i>`},
-
+let conhecimentos = {
+  'linguagenp': [{ 'nome': "Javascript", 'nivel': 7 },
+  { 'nome': "C/C++", 'nivel': 4 },
+  { 'nome': "PHP", 'nivel': 6 },
+  { 'nome': "R", 'nivel': 2 },
+  { 'nome': "Python", 'nivel': 2 },
+  { 'nome': "Java", 'nivel': 1 }
+  ],
+  'bancodedados': [{ 'nome': "Mysql", 'nivel': 6 }, { 'nome': "orientdb", 'nivel': 4 }, { 'nome': "counchdb", 'nivel': 4 }],
+  'linguas': [{ 'nome': "Inglês", 'nivel': 2 }, { 'nome': "Espanhol", 'nivel': 5 }],
+  'tecnicos': [
+    { 'nome': "Manutenção e montagem de eletronicos", 'nivel': 7 },
+    { 'nome': "Engenharia de software", 'nivel': 9, 'descricao': "metodologias ageis como kamban e scrum e outras tecnicas de desenvolvimento" },
+    { 'nome': "Algoritmos", 'nivel': 10 },
+    { 'nome': "Estrutura de dados", 'nivel': 8 },
+    { 'nome': "Redes neurais", 'nivel': 2 }
+  ]
+};
+let contatos = {
+  'Linkedin': { 'link': "https://www.linkedin.com/in/joão-vitor-leite-363855231/", 'logo': `<i class="fa-brands fa-linkedin"></i>` },
+  'Github': { 'link': "https://github.com/Joao-vls", 'logo': `<i class="fa-brands fa-github"></i>` },
+  'Instagram': { 'link': "https://www.instagram.com/dev.leite/", 'logo': `<i class="fa-brands fa-square-instagram"></i>` },
+  'Twitter': { 'link': "https://twitter.com/desleite", 'logo': `<i class="fa-brands fa-square-twitter"></i>` },
 };
